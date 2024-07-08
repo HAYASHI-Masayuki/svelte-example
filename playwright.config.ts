f12de38d1a641f260467286b4d74770b6339f02b
HAYASHI Masayuki <masayuki.hayashi.mcc@gmail.com>
2024-07-08 21:27:31 +0900

    1.45.1に戻して、--enable-gpuが原因じゃないか試してみた(が、そうではなさそう)

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,13 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+        launchOptions: {
+          args: ['--use-angle'],
+          ignoreDefaultArgs: ['--enable-gpu'],
+        },
+      },
     },
 
     {
