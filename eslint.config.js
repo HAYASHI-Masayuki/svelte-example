4775342904465a130d842315aaf089ef327aa87f
HAYASHI Masayuki <masayuki.hayashi.mcc@gmail.com>
2024-08-02 21:07:17 +0900

    eslintPluginSvelteを使うように

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,13 @@
 import globals from "globals"
 import pluginJs from "@eslint/js"
 import tseslint from "typescript-eslint"
+import eslintPluginSvelte from "eslint-plugin-svelte"
 
 export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  ...eslintPluginSvelte.configs['flat/recommended'],
   {
     rules: {
       semi: ['error', 'never'],
